6f0a8f908606c676099c974c44b01b31b1304c7f
Lucas Borges Moraes <lucasborgesmoraes@gmail.com>
2020-04-15 19:21:56 -0300

    JWT

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+
+const jwt = require('jsonwebtoken');
+app.use(express.json()); //deixa o app usar JSON
+
 
 const uri = "mongodb+srv://Lucasbm777:senhasenha@data-nwe37.mongodb.net/test?retryWrites=true&w=majority"
 
@@ -14,40 +20,67 @@ mongoose.connect(uri);
 MongoClient.connect(uri, (err, db) => {
     if (err) return console.log(err)
     var dbo = db.db("test");
-    
+
     db.close();
-    
+
 })
 
 
-app.listen(3000 ,()=> {
+app.listen(3000, () => {
     console.log('running at 3000')
 });
 
 var userSchema = new Schema({
     "Username": String,
-    "Password": String, 
+    "Password": String,
 })
 
 var User = mongoose.model('User', userSchema);
 
-app.use(bodyParser.urlencoded({extend: false }));
+app.use(bodyParser.urlencoded({ extend: false }));
 app.use(bodyParser.json());
 
 
-app.get('/', async (req,res)=>{
-   console.log('hello mundo')
-  res.send('hello world')
-  await auth();
+app.get('/', async (req, res) => {
+    console.log('hello mundo')
+    res.send('hello world')
+    await auth();
 
 })
 
-app.post('/posts',(req,res)=>{
+const post = [
+    {
+        username: 'Lucas',
+        password: 'senha123'
+    },
+    {
+        username: 'caio',
+        password: 'senha1234'
+    }
+];
+
+app.get('/log', authenticateToken, (req, res) => {
+    res.json(post.filter(post => post.username === req.user.name))
+})
+
+
+
+//login com JWT
+app.post('/login', (req, res) => {
+    //authentica user:
+    const username = req.body.username
+    const user = { name: username }
+
+    const acessToken = jwt.sign(user, process.env.token)
+    res.json({ acessToken: acessToken })
+})
+
+app.post('/posts', (req, res) => {
     var myData = new User({
         Username: req.body.Username,
         Password: req.body.Password
-      });
-      console.log(myData.Username);
+    });
+    console.log(myData.Username);
     myData.save()
         .then(item => {
             console.log(req.body)
@@ -69,17 +102,29 @@ app.post('/posts',(req,res)=>{
 //        const queryUser =  dbo.collection("users").findOne(query).exec( function(err, user ){
 //            console.log(user);
 //        });
-      
+
 //     })} 
 //formik = completo mas complexo
 
-async function auth(){
+async function auth() {
     const userFromDb = await User.findOne({ Username: 'Millena', Password: '12345' });
     console.log(userFromDb);
-    if(userFromDb == null){
+    if (userFromDb == null) {
         console.log('o sistema não encontrou usuários/senha na data base.');
     }
 
 }
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401) //token não encontrado
+
+    jwt.verify(token, process.env.token, (err, user)=>{
+        if (err) return res.sendStatus(403) //Token encontrado mas não correto
+        req.user =  user
+        next()
+    })
+}
+
 
 //Shcma definido da pra usar o método acima.
